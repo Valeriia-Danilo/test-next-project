96abfc6ba4b7bc280fc43067ec809aec7150dfae
Valeriia Danylo <159473200+Valeriia-Danilo@users.noreply.github.com>
2025-10-26 17:34:24 +0100

    add navigation and fatchin notes list

---
+++ app/notes/page.tsx
@@ -0,0 +1,15 @@
+import NoteList from "@/components/NoteList/NoteList";
+import { getNotes } from "@/lib/api";
+
+const Notes = async () => {
+    const res = await getNotes();
+
+    return (
+        <section>
+            <h1>Notes List</h1>
+            {res?.notes?.length > 0 && <NoteList notes={res.notes } />}
+        </section>
+);
+}
+
+export default Notes;
