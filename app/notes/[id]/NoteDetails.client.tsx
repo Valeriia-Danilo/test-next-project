75611e9fd9b968680ffc4047720a03b30259c437
Valeriia Danylo <159473200+Valeriia-Danilo@users.noreply.github.com>
2025-12-02 12:16:56 +0100

    add Back button

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,38 +1,52 @@
-"use client";
+'use client';
 
-import { useQuery } from "@tanstack/react-query";
-import { useParams } from "next/navigation";
-import { getOneNote } from "@/lib/api";
-import Loading from "@/app/loading";
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'next/navigation';
+import { getOneNote } from '@/lib/api';
+import Loading from '@/app/loading';
+import { useRouter } from 'next/navigation';
 
 const NoteDetailsClient = () => {
-    const { id } = useParams<{ id: string }>();
-    const { data: note, isLoading, error } = useQuery({
-        queryKey: ["note", id],
-        queryFn: () => getOneNote(id),
-        refetchOnMount: false,
-    });
+  const router = useRouter();
+  const { id } = useParams<{ id: string }>();
+  const {
+    data: note,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['note', id],
+    queryFn: () => getOneNote(id),
+    refetchOnMount: false,
+  });
 
-    if (isLoading) return <Loading/>;
+  if (isLoading) return <Loading />;
 
-    if (error || !note) return <p>Some error...</p>;
+  if (error || !note) return <p>Some error...</p>;
 
-         const formattedDate = note.updatedAt
+  const formattedDate = note.updatedAt
     ? `Updated at: ${note.updatedAt}`
-        : `Created at: ${note.createdAt}`;
-    
-    const handleClick = () => {
-        console.log(note.title);
+    : `Created at: ${note.createdAt}`;
+
+  const handleClick = () => {
+    console.log(note.title);
+  };
+
+  const handleGoBack = () => {
+    const isSure = confirm('Are you sure?');
+    if (isSure) {
+      router.back();
     }
-    
-         return (
+  };
+
+  return (
     <div>
+      <button onClick={handleGoBack}>Back</button>
       <h2>{note.title}</h2>
       <p>{note.content}</p>
       <button onClick={handleClick}>Edit</button>
       <p>{formattedDate}</p>
     </div>
   );
-}
+};
 
 export default NoteDetailsClient;
