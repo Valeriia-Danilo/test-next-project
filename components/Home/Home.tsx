21e46e045599e79328542964793008eddd4beec3
Valeriia Danylo <159473200+Valeriia-Danilo@users.noreply.github.com>
2025-12-10 19:15:47 +0100

    add images

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,23 +1,37 @@
-import css from "./Home.module.css";
+import Image from 'next/image';
+import css from './Home.module.css';
 
 const Home = () => {
   return (
-   <main className={css.main}> 
-  <div className={css.container}>
-    <h1 className={css.title}>Welcome to NoteHub</h1>
-    <p className={css.description}>
-      NoteHub is a simple and efficient application designed for managing
-      personal notes. It helps keep your thoughts organized and accessible
-      in one place, whether you are at home or on the go.
-    </p>
-    <p className={css.description}>
-      The app provides a clean interface for writing, editing, and browsing
-      notes. With support for keyword search and structured organization,
-      NoteHub offers a streamlined experience for anyone who values clarity
-      and productivity.
-    </p>
-  </div>
-</main>
+    <main className={css.main}>
+      <div className={css.container}>
+        <h1 className={css.title}>Welcome to NoteHub</h1>
+        <p className={css.description}>
+          NoteHub is a simple and efficient application designed for managing
+          personal notes. It helps keep your thoughts organized and accessible
+          in one place, whether you are at home or on the go.
+        </p>
+        <p className={css.description}>
+          The app provides a clean interface for writing, editing, and browsing
+          notes. With support for keyword search and structured organization,
+          NoteHub offers a streamlined experience for anyone who values clarity
+          and productivity.
+        </p>
+        <Image
+          src="/133779521637837271.jpg"
+          alt="Home pege image"
+          width={600}
+          height={300}
+          priority
+        />
+        <Image
+          src="https://picsum.photos/seed/picsum/300/300"
+          alt="Home pege image"
+          width={600}
+          height={300}
+        />
+      </div>
+    </main>
   );
 };
 
