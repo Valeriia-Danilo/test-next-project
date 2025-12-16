e9061d968051c3484a11bee511370f2a5f27106a
Valeriia Danylo <159473200+Valeriia-Danilo@users.noreply.github.com>
2025-12-16 12:17:11 +0100

    add cach

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,20 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ protocol: 'https', hostname: 'picsum.photos' }],
   },
+  async headers() {
+    return [
+      {
+        source: '/notes/filter/[...slug]',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=300, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
